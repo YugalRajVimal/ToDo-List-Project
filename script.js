aa48676bbda81ecd2a49f9565282c3e79a50541d
YugalRajVimal <sagaryugal472@gmail.com>
2023-07-13 01:33:26 +0530

    Add Task, Delete Task, Toggle Task and Render List functions Updated

--- script.js
+++ script.js
@@ -1,23 +1,63 @@
-const tasks = [];
+let tasks = [];
 const taskList = document.getElementById('list');
 const addTaskInput = document.getElementById('add');
 const tasksCounter = document.getElementById('tasks-counter');
 
 console.log('Working');
 
-function renderList () {
-	for(var i=0;i<tasks.length;i++){
-		console.log(tasks[i].id,tasks[i].text,'is',tasks[i].done);
-	}
+function addTaskToDOM (task) {
+	const li = document.createElement('li');
+
+	li.innerHTML=`
+		<input type="checkbox" id="${task.id}" ${task.done ? 'checked' : ''} class="custom-checkbox">
+        <label for="${task.id}">${task.text}</label>
+        <img src="bin.svg" class="delete" data-id="${task.id}" />
+	`
+	taskList.append(li);
 }
 
-function markTaskAsComplete (taskId) {}
+function renderList () {
+	taskList.innerHTML = '';
 
-function deleteTask (taskId) {}
+	for(var i=0;i<tasks.length;i++){
+		addTaskToDOM(tasks[i]);
+	}
+
+	tasksCounter.innerHTML = tasks.length;
+}
+
+function toggleTask(taskId) {
+	const task=tasks.filter(function (task){
+		return task.id === taskId;
+	})
+	if(task.length > 0){
+		const currentTask=task[0];
+		currentTask.done=!currentTask.done;
+		renderList();
+		showNotification('Task Toggled Successfully');
+		return;
+	}
+	showNotification('Could not toggle the Task');
+}
+
+function deleteTask (taskId) {
+	const newTasks=tasks.filter(function (task){
+		return task.id !== taskId;
+	})
+
+	tasks=newTasks;
+	renderList();
+	showNotification('Task Deleted Successfully');
+}
 
 function addTask (task) {
-	tasks[tasks.length]=task;
-	renderList();
+	if(task){
+		tasks.push(task);
+		renderList();
+		showNotification('Task Added Successfully');
+		return;
+	}
+	showNotification('Task can not be added');
 }
 
 function showNotification(text) {
