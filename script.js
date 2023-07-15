11bb1940c19ac6767f71ad78e1859dfa370a8cc5
YugalRajVimal <sagaryugal472@gmail.com>
2023-07-15 21:55:54 +0530

    Fethched Data from an API

--- script.js
+++ script.js
@@ -3,14 +3,27 @@ const taskList = document.getElementById('list');
 const addTaskInput = document.getElementById('add');
 const tasksCounter = document.getElementById('tasks-counter');
 
-console.log('Working');
+function fetchTodos(){
+	//Get Request
+	fetch('https://jsonplaceholder.typicode.com/todos')
+	.then(function(response){
+		console.log(response);
+		return response.json();
+	}).then(function(data){
+		tasks=data.slice(0,10);
+		renderList();
+	})
+	.catch(function(error){
+		console.log('error',error);
+	})
+}
 
 function addTaskToDOM (task) {
 	const li = document.createElement('li');
 
 	li.innerHTML=`
-		<input type="checkbox" id="${task.id}" ${task.done ? 'checked' : ''} class="custom-checkbox">
-        <label for="${task.id}">${task.text}</label>
+		<input type="checkbox" id="${task.id}" ${task.completed ? 'checked' : ''} class="custom-checkbox">
+        <label for="${task.id}">${task.title}</label>
         <img src="bin.svg" class="delete" data-id="${task.id}" />
 	`
 	taskList.append(li);
@@ -28,11 +41,11 @@ function renderList () {
 
 function toggleTask(taskId) {
 	const task=tasks.filter(function (task){
-		return task.id === taskId;
+		return task.id === Number(taskId);
 	})
 	if(task.length > 0){
 		const currentTask=task[0];
-		currentTask.done=!currentTask.done;
+		currentTask.completed=!currentTask.completed;
 		renderList();
 		showNotification('Task Toggled Successfully');
 		return;
@@ -42,7 +55,7 @@ function toggleTask(taskId) {
 
 function deleteTask (taskId) {
 	const newTasks=tasks.filter(function (task){
-		return task.id !== taskId;
+		return task.id !== Number(taskId);
 	})
 
 	tasks=newTasks;
@@ -74,9 +87,9 @@ function handleInputKeypress(e){
 		}
 
 		const task= {
-			text, //text: text
-			id: Date.now().toString(),
-			done: false
+			title: text,
+			id: Date.now(),
+			completed: false
 		}
 
 		e.target.value='';
@@ -100,6 +113,7 @@ function handleClickListener(e){
 }
 
 function initializeApp(){
+	fetchTodos();
 	addTaskInput.addEventListener('keyup',handleInputKeypress);
 	document.addEventListener('click',handleClickListener);
 }
